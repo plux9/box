b832f33840676f829b7b68b00862f04ccbd00b76
plux9 <plux9@users.noreply.github.com>
2014-08-01 09:54:00 -0400

    Apply .gitignore and install tox

--- box/tests/test_views.py
+++ box/tests/test_views.py
@@ -1,17 +1,14 @@
-import unittest
-
 from pyramid import testing
 
-
-class ViewTests(unittest.TestCase):
-    def setUp(self):
+class TestViewClass():
+    def setup_method(self, method):
         self.config = testing.setUp()
 
-    def tearDown(self):
+    def teardown_method(self, method):
         testing.tearDown()
 
     def test_my_view(self):
-        from .views import my_view
+        from box.views import my_view
         request = testing.DummyRequest()
         info = my_view(request)
-        self.assertEqual(info['project'], 'box')
+        assert info['project'] == 'box'
