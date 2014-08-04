e050c69e21416180cd2fe6fc37cfeebe23221193
plux9 <plux9@users.noreply.github.com>
2014-08-04 11:07:44 -0400

    Integrate setup.py with tox

--- setup.py
+++ setup.py
@@ -1,6 +1,23 @@
 import os
 
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+import sys
+
+class Tox(TestCommand):
+    user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.tox_args = ''
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+    def run_tests(self):
+        import tox
+        import shlex
+        errno = tox.cmdline(args=shlex.split(self.tox_args))
+        sys.exit(errno)
 
 here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.txt')) as f:
@@ -36,6 +53,7 @@ setup(name='box',
       install_requires=requires,
       tests_require=requires,
       test_suite="box",
+      cmdclass = {'test': Tox},
       entry_points="""
       [paste.app_factory]
       main = box:main
